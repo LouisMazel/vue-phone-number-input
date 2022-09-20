87da85d9aa5213b696b0ab1145112d05d9ac2603
LouisMazel <mazuel.loic@gmail.com>
2022-09-20 14:59:14 +0200

    chore: tsconfig & eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     'vue/order-in-components': 'error',
     'vue/this-in-template': 'error',
     'vue/script-indent': 'error',
+    'vue/multi-word-component-names': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
 
